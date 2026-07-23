a56d8c011cb744f6371dc37d7bc401b439d46eb2
Natasha Coulter <natashacoulter@gmail.com>
2026-07-23 14:11:01 -0400

    Add .gitignore, stop tracking node_modules and .DS_Store

---
+++ lesson_07/express-ts-server/src/modules/todos/todo.containers.ts
@@ -0,0 +1,15 @@
+// writing - соединим все независимые слои
+
+import { Router } from "express";
+import { InMemoryRepository } from "./todo.repository";
+import { TodoService } from "./todo.service";
+import { TodoController } from "./todo.controller";
+import { createTodoRouter } from "./todo.router";
+
+export function buildTodoRouter(): Router {
+  const repo = new InMemoryRepository();
+  const service = new TodoService(repo);
+  const controller = new TodoController(service);
+
+  return createTodoRouter(controller);
+}
